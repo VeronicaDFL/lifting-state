7ef02e84ae083d34c8c2461705a7bf2e82191015
Veronica De Felice <verodefelic@gmail.com>
2023-04-20 11:00:43 -0400

    Set Up loging Users

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+export default  function Login({setUser}) {
+
+
+    return (
+    <>
+    <h2>Login</h2>
+    <button onClick ={() => setUser ('Returning User')}>Login</button>
+    &nbsp;
+    <button onClick = {() => setUser()}>Log Out</button>
+    </>
+
+    )
+}
---
+++ src/components/Signup.jsx
@@ -0,0 +1,10 @@
+export default  function Signup({setUser}) {
+
+
+    return(
+    <>
+    <h2>Sign Up</h2>
+    <button onClick={() => setUser(' New User')}>Sign Up</button>
+    </>
+    )
+}
